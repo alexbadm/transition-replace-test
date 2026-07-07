72eb4a040e14e364a2d6c297c22165f4c1db3110
Alex Badmashkaev <eto.ya@list.ru>
2026-07-07 17:38:06 +0300

    Always main show is children

--- src/TransitionContainer/TransitionContainer.tsx
+++ src/TransitionContainer/TransitionContainer.tsx
@@ -3,6 +3,7 @@ import {
   FC,
   ReactElement,
   useEffect,
+  useLayoutEffect,
   useRef,
   useState,
 } from "react";
@@ -12,33 +13,69 @@ import "./TransitionContainer.css";
  * Описание алгоритма TransitionContainer
  *
  * TransitionContainer при монтировании получает элемент-потомок, отрисовывает как обычно.
- * В prevChildren записывается копия children, показываем копию.
+ * В prevChildren записывается копия children, но показываем children. В случае перерисовки глубоко
+ * в потомке мы это не увидим, и будем показывать устаревшую копию.
  *        container - height:auto
- *        transitionPrevNode - position:static, opacity:1
- *        transitionNode - position:static, opacity:0, display:none
+ *        transitionPrevNode - position:absolute, opacity:0
+ *        transitionNode - position:static, opacity:1
+ *        transitionDuration: 0ms
+ *        status: END
  * При изменении потомка:
  *   1. контейнеру присваивается высота предыдущего потомка, а position у потомков становится absolute
  *        container - static height
  *        transitionPrevNode - position:absolute, opacity:1
- *        transitionNode - position:absolute, opacity:0, display:block
+ *        transitionNode - position:absolute, opacity:0
+ *        transitionDuration: 0ms
+ *        status: START
  *   2. предыдущему потомку плавно меняется opacity 1->0, а новому потомку плавно меняется opacity 0->1,
  *      а высота контейнера плавно меняется к значению высоты нового потомка
  *        container - static height (new value)
  *        transitionPrevNode - position:absolute, opacity:0
- *        transitionNode - position:absolute, opacity:1, display:block
- *   3. после завершения анимации записываем копию children, его position становится static, а высота контейнера auto
+ *        transitionNode - position:absolute, opacity:1
+ *        transitionDuration: {duration}ms
+ *        status: TRANSITIONING
+ *   3. после завершения анимации записываем копию children, position у children становится static, а высота контейнера auto
  *        container - height:auto
- *        transitionPrevNode - position:static, opacity:1
- *        transitionNode - position:static, opacity:0, display:none
+ *        transitionPrevNode - position:absolute, opacity:0
+ *        transitionNode - position:static, opacity:1
+ *        transitionDuration: 0ms
+ *        status: END
  */
 
 const defaultDuration = 600;
 
 interface TransitionContainerProps {
+  /**
+   * Сменяемый элемент-потомок. Обязательно должен иметь уникальный `key`!
+   */
   children: ReactElement;
+  /**
+   * Продолжительность анимации в миллисекундах.
+   * @default 600
+   */
   duration?: number;
 }
 
+/**
+ * TransitionContainer используется для плавной смены компонента на странице.
+ * Принимает один единственный children элемент, который может быть заменёт другим.
+ *
+ * Важно! Для работы плавной смены потомка, сменяемые потомки должны иметь уникальные `key`!
+ *
+ * @example
+ *
+ * <TransitionContainer duration={1600}>
+ *   {state === "alpha" ? (
+ *     <ComponentAlpha key="alpha" />
+ *   ) : state === "beta" ? (
+ *     <ComponentBeta key="beta" />
+ *   ) : (
+ *     <ComponentGamma key="gamma" />
+ *   )}
+ * </TransitionContainer>
+ *
+ * @kind component
+ */
 export const TransitionContainer: FC<TransitionContainerProps> = ({
   children,
   duration = defaultDuration,
@@ -57,12 +94,13 @@ export const TransitionContainer: FC<TransitionContainerProps> = ({
     status: "END",
   }));
 
-  useEffect(() => {
+  useLayoutEffect(() => {
     setState((prev) => {
       return prev.status !== "END" || prev.prevChildren.key !== children.key
         ? {
             ...prev,
-            containerHeight: prevNodeRef.current?.scrollHeight,
+            containerHeight:
+              prevNodeRef.current?.getBoundingClientRect().height,
             status: "START",
           }
         : prev;
@@ -73,7 +111,7 @@ export const TransitionContainer: FC<TransitionContainerProps> = ({
     if (state.status === "START") {
       setState((prev) => ({
         ...prev,
-        containerHeight: nodeRef.current?.scrollHeight,
+        containerHeight: nodeRef.current?.getBoundingClientRect().height,
         status: "TRANSITIONING",
       }));
     } else if (state.status === "TRANSITIONING") {
@@ -105,9 +143,8 @@ export const TransitionContainer: FC<TransitionContainerProps> = ({
         ref={prevNodeRef}
         className="transitionPrevNode"
         style={{
-          transitionDuration: `${state.status === "END" ? 0 : duration}ms`,
-          opacity: Number(state.status !== "TRANSITIONING"),
-          position: state.status === "END" ? "static" : "absolute",
+          transitionDuration: `${state.status === "TRANSITIONING" ? duration : 0}ms`,
+          opacity: Number(state.status === "START"),
         }}
       >
         {state.prevChildren}
@@ -116,9 +153,9 @@ export const TransitionContainer: FC<TransitionContainerProps> = ({
         ref={nodeRef}
         className="transitionNode"
         style={{
-          transitionDuration: `${state.status === "END" ? 0 : duration}ms`,
-          opacity: Number(state.status === "TRANSITIONING"),
-          display: state.status === "END" ? "none" : "block",
+          transitionDuration: `${state.status === "TRANSITIONING" ? duration : 0}ms`,
+          opacity: Number(state.status !== "START"),
+          position: state.status === "END" ? "static" : "absolute",
         }}
       >
         {children}
